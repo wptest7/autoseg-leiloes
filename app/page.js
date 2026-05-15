8cd23c5273845e908c22ddaa65144733cbbcf9ee
wptest7 <wendelsinistro77@gmail.com>
2026-05-14 23:53:20 -0300

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <div style={{background: "black", color: "white", minHeight: "100vh", display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column"}}>
+      <h1 style={{fontSize: "40px"}}>AUTO SEG</h1>
+      <p style={{color: "yellow"}}>LEILÕES PREMIUM</p>
+
+      <a
+        href="https://wa.me/5517982159410"
+        style={{marginTop: "20px", padding: "10px 20px", background: "green", color: "white", textDecoration: "none", borderRadius: "10px"}}
+      >
+        FALAR NO WHATSAPP
+      </a>
+    </div>
+  );
+}
